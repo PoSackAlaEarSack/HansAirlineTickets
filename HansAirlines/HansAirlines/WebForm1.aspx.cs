911d51313823a567e701bf405fd7ff9f7ee9fa43
PoSackAlaEarSack <posackalaearsack@gmail.com>
2018-01-31 10:18:00 -0500

    I just put in some work and some db stuff but none of it works and we gotta change it a lot

---
+++ HansAirlines/HansAirlines/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace HansAirlines
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection(Server = localhost; uid = stefan; pwd = 123; database = blah;);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
